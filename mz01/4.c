e15ce647f5fcee8b4807851709d57065fb531456
gerarte04 <gerarte04@gmail.com>
2023-09-17 22:31:26 +0300

    update mz01/4.c

--- mz01/4.c
+++ mz01/4.c
@@ -1,14 +1,32 @@
 #include <stdlib.h>
 
-int comp(const void *x, const void *y) {
-    int a = *(int*) x;
-    int b = *(int*) y;
-    int ar = abs(a % 2);
-    int br = abs(b % 2);
+int
+comp(const void *x, const void *y)
+{
+    int a = *(int *) x;
+    int b = *(int *) y;
+    int ar = a & 1;
+    int br = b & 1;
 
-    return (ar == br) ? (1 - ar - br) * ((a > b) - (a < b)) : ar - br;
+    if (ar != br) {
+        return ar - br;
+    } else if (a == b) {
+        return 0;
+    } else if (ar == 0) {
+        if (a < b) {
+            return -1;
+        }
+
+        return 1;
+    } else if (b < a) {
+        return -1;
+    }
+
+    return 1;
 }
 
-void sort_even_odd(size_t count, int *data) {
+void
+sort_even_odd(size_t count, int *data)
+{
     qsort(data, count, sizeof(int), comp);
 }
