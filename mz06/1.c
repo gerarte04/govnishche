ee7af86ea60ae7a6e4404cce38d5a1dd92771c10
gerarte04 <gerarte04@gmail.com>
2023-10-13 22:02:13 +0300

    resolving rejects mz06

--- mz06/1.c
+++ mz06/1.c
@@ -1,10 +1,25 @@
 #include <dirent.h>
 #include <linux/limits.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+int
+check_ext(char *buf)
+{
+    int len = strlen(buf);
+
+    return len >= 4 && strcmp(buf + len - 4, ".exe") == 0;
+}
+
+int
+check_cont(int buf_len, char *str1, char *str2)
+{
+    return buf_len > strlen(str1) + strlen(str2) + 1;
+}
 
 int
 main(int argc, char **argv)
@@ -12,25 +27,26 @@ main(int argc, char **argv)
     DIR *dir = opendir(argv[1]);
     struct dirent *dd;
 
+    char buf[PATH_MAX];
+    int buf_len = sizeof(buf) / sizeof(buf[0]);
+
     int cnt = 0;
 
     while ((dd = readdir(dir)) != NULL) {
-        char buf[PATH_MAX + 1];
-        snprintf(buf, PATH_MAX, "%s/%s", argv[1], dd->d_name);
+        snprintf(buf, buf_len, "%s/%s", argv[1], dd->d_name);
 
         struct stat s;
-        int len;
 
-        int check_ext = ((len = strlen(buf)) >= 4 && strcmp(buf + len - 4, ".exe") == 0);
-
-        if (stat(buf, &s) == 0 && S_ISREG(s.st_mode) && access(buf, X_OK) == 0 && check_ext) {
-            cnt++;
+        if (check_cont(buf_len, argv[1], dd->d_name)) {
+            if (stat(buf, &s) == 0 && S_ISREG(s.st_mode) && access(buf, X_OK) == 0 && check_ext(&buf[0])) {
+                cnt++;
+            }
         }
     }
 
-    printf("%d\n", cnt);
-
     closedir(dir);
+
+    printf("%d\n", cnt);
 
     return 0;
 }
--- mz06/2.c
+++ mz06/2.c
@@ -2,42 +2,41 @@ void
 normalize_path(char *buf)
 {
     char *str = buf;
+    char *ptr = buf;
 
-    while (*buf != '\0') {
-        if (*buf == '/') {
-            if (*(buf + 1) == '\0' && buf != str) {
-                *buf = '\0';
-            }
+    while (buf[0] != '\0') {
+        if (buf[0] == '/') {
+            if (buf[1] == '.') {
+                if (buf[2] == '.' && (buf[3] == '/' || buf[3] == '\0')) {
+                    buf += 2;
 
-            if (*(buf + 1) == '.') {
-                int go_cur = *(buf + 2) == '/' || *(buf + 2) == '\0';
-                int go_back = *(buf + 2) == '.' && (*(buf + 3) == '/' || *(buf + 3) == '\0');
-
-                if (go_cur || go_back) {
-                    char *src = (go_cur) ? buf + 2 : buf + 3;
-                    char *dst = buf;
-
-                    if (go_back && buf != str) {
+                    if (ptr != str) {
                         do {
-                            dst--;
-                        } while (*dst != '/');
+                            ptr--;
+                        } while (ptr[0] != '/');
                     }
 
-                    buf = dst - 1;
-
-                    if (*src == '\0') {
-                        *(dst + 1) = '\0';
-                    } else {
-                        while (*(src - 1) != '\0') {
-                            *dst = *src;
-                            dst++;
-                            src++;
-                        }
-                    }
+                    ptr--;
+                } else if (buf[2] == '/' || buf[2] == '\0') {
+                    buf++;
+                    ptr--;
                 }
             }
         }
 
+        ptr++;
         buf++;
+        ptr[0] = buf[0];
+    }
+
+    if (*(buf - 1) == '/') {
+        *(ptr - 1) = '\0';
+    } else {
+        ptr[0] = '\0';
+    }
+
+    if (str[0] == '\0') {
+        str[0] = '/';
+        str[1] = '\0';
     }
 }
