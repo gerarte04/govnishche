62d2a65b431b5e38c71024512dc5b549bc99446c
gerarte04 <gerarte04@gmail.com>
2023-11-29 15:15:42 +0300

    mz13: fixed prac.c, added tasks.md

--- mz13/prac.c
+++ mz13/prac.c
@@ -1,12 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/ipc.h>
+#include <sys/msg.h>
 #include <sys/sem.h>
 #include <sys/shm.h>
-#include <sys/msg.h>
-#include <string.h>
-#include <stdio.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <stdlib.h>
 
 // while (!semctl(id, 0, GETVAL, 0)) {
 //     semctl(id, 0, SETVAL, 0);  - так работать нельзя (операция не атомарная)
@@ -58,22 +58,34 @@ int f1(int argc, char **argv)
     return 0;
 }
 
-int main(int argc, char **argv) // не работает
+int main(int argc, char **argv)
 {
     int nproc = strtol(argv[1], NULL, 10);
     int xmax = strtol(argv[2], NULL, 10);
 
     int semid = semget(IPC_PRIVATE, 10, IPC_CREAT | 0600);
     int shmid = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0600);
+    
     int *x = shmat(shmid, NULL, 0);
     *x = 1;
 
+    struct sembuf psems[nproc];
+    struct sembuf vsems[nproc];
+
+    for (int i = 0; i < nproc; i++) {
+        psems[i] = (struct sembuf) { i, -1, 0 };
+        vsems[i] = (struct sembuf) { i, 1, 0 };
+    }
+
+    semop(semid, &vsems[0], 1);
+
     for (int i = 0; i < nproc; i++) {
         if (!fork()) {
-            while (1) {
+            while (semop(semid, &psems[i], 1) >= 0) {
                 printf("%d %d\n", i + 1, *x);
 
                 if (*x == xmax) {
+                    printf("done\n");
                     semctl(semid, 0, IPC_RMID, 0);
                     shmctl(shmid, IPC_RMID, 0);
                     exit(0);
@@ -81,20 +93,14 @@ int main(int argc, char **argv) // не работает
 
                 (*x)++;
 
-                struct sembuf v = { (i == nproc - 1 ? 0 : i + 1), -1, 0 };
-
-                if (semop(semid, &v, 1) < 0) {
-                    exit(0);
-                }
-
-                struct sembuf p = { i, 1, 0 };
-                semop(semid, &p, 1);
+                semop(semid, &vsems[i == nproc - 1 ? 0 : i + 1], 1);
             }
+
+            exit(0);
         }
     }
 
     while (wait(NULL) != -1) {
-
     }
 
     return 0;
