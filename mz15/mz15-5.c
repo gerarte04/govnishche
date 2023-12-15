90fd18ea16151909d62d8584a4f09200f1e06770
gerarte04 <gerarte04@gmail.com>
2023-12-15 23:30:52 +0300

    solved mz15-5 within 175932579375 attempts

--- mz15/mz15-5.c
+++ mz15/mz15-5.c
@@ -22,14 +22,23 @@ main(int argc, char **argv)
     sigset_t s;
     sigemptyset(&s);
     sigaddset(&s, SIGTERM);
-    sigaddset(&s, SIGRTMIN);
+
+    for (int i = SIGRTMIN; i <= SIGRTMAX; i++) {
+        sigaddset(&s, i);
+    }
+
     sigprocmask(SIG_BLOCK, &s, NULL);
 
     long long sum[FILES_MAXCNT + 1] = {};
-    int nfile = 0;
 
     struct pollfd fds[2] = {(struct pollfd){-1, POLLIN, 0}, (struct pollfd){signalfd(-1, &s, 0), POLLIN, 0}};
     struct signalfd_siginfo sinfo = {};
+    int nfile = 0;
+    int fd_list[FILES_MAXCNT + 1];
+
+    for (int i = 1; i < argc; i++) {
+        fd_list[i] = open(argv[i], O_RDONLY);
+    }
 
     printf("%d\n", getpid());
     fflush(stdout);
@@ -40,30 +49,25 @@ main(int argc, char **argv)
         if (fds[1].revents & POLLIN) {
             read(fds[1].fd, &sinfo, sizeof(sinfo));
 
-            if (sinfo.ssi_signo == SIGRTMIN) {
-                close(fds[0].fd);
-
-                if (nfile < argc - 1 && nfile < FILES_MAXCNT) {
-                    nfile++;
-                    fds[0].fd = open(argv[nfile], O_RDONLY);
-                }
+            if (sinfo.ssi_signo >= SIGRTMIN && sinfo.ssi_signo <= SIGRTMAX) {
+                nfile = sinfo.ssi_signo - SIGRTMIN + 1;
+                fds[0].fd = fd_list[nfile];
             }
         } else if (fds[0].revents & POLLIN) {
             char buf[BSIZE + 1];
             buf[BSIZE] = '\0';
 
-            if (read(fds[0].fd, buf, BSIZE) == 0) {
-                close(fds[0].fd);
-                continue;
+            if (read(fds[0].fd, buf, BSIZE) > 0) {
+                long long x;
+                sscanf(buf, "%lld", &x);
+                sum[nfile] += x;
             }
-
-            long long x;
-            sscanf(buf, "%lld", &x);
-            sum[nfile] += x;
         }
     }
 
-    close(fds[0].fd);
+    for (int i = 1; i < argc; i++) {
+        close(fd_list[i]);
+    }
 
     for (int i = 1; i < argc; i++) {
         printf("%lld\n", sum[i]);
